4b865b03823fe63715534ac9d0e014d8ae04afbb
robertwtm <robertwtavares@gmail.com>
2021-11-09 18:50:24 -0300

    Classe TestaCaracteres adicionada para a aula Char e String

---
+++ java/src/SintaxeVariaveisFluxo/TestaCaracteres.java
@@ -0,0 +1,22 @@
+package SintaxeVariaveisFluxo;
+
+public class TestaCaracteres {
+	
+	public static void main (String[] args) {
+		char letra = 'A';
+		System.out.println(letra);
+		
+		char valor = 66;
+		System.out.println(valor);
+		
+		valor = (char)(valor+1);
+		System.out.println(valor);
+		
+		String palavra = "alura curso online de tecnologia";
+		System.out.println(palavra);
+				
+		palavra = palavra + 2020;
+		System.out.println(palavra);
+		
+	}
+}
