1c438cf717034150a066c308b14ea69957cddafa
robertwtm <robertwtavares@gmail.com>
2021-11-09 18:11:00 -0300

    Organizando as astividades do curso da alura em Pacotes

--- java/src/SintaxeBasica/Programa.java
+++ java/src/SintaxeBasica/Programa.java
@@ -1,3 +1,4 @@
+package SintaxeBasica;
 
 public class Programa {
 	
---
+++ java/src/SintaxeVariaveisFluxo/TestaVariaveis.java
@@ -0,0 +1,26 @@
+package SintaxeVariaveisFluxo;
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Ola novamente");
+		
+		int idade;
+		idade = 37;
+
+		System.out.println(idade);
+
+		idade = 30 + 10;
+
+		System.out.println(idade);
+
+		idade = (7 * 5) +  2;
+
+		System.out.println(idade);
+		
+		System.out.println("A idade é: " + idade);
+	}
+
+}
