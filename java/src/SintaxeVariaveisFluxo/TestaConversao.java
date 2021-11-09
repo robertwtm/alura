7bfd9d05159481b0de93ef52dd9b9b9c40408ebb
robertwtm <robertwtavares@gmail.com>
2021-11-09 18:37:18 -0300

    Conversões e outros tipos

---
+++ java/src/SintaxeVariaveisFluxo/TestaConversao.java
@@ -0,0 +1,19 @@
+package SintaxeVariaveisFluxo;
+
+public class TestaConversao {
+	
+	public static void main(String[] args) {
+		double salario = 1270.50;
+		int valor = (int)salario;
+		long numeroGrande = 32432423523L;
+		short valorPequeno = 2131;
+		byte b = 127;
+		double valor1 = 0.2;
+		double valor2 = 0.1;
+		double total = valor1+valor2;
+		float pontoFlutuante = 3.14f;
+		
+		System.out.println(valor);
+		
+	}
+}
